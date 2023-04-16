c4494708aa70b084d7dec7076a5c8399740f9c53
udhay <udhay@LAPTOP-FRFRGCHU>
2023-04-15 23:42:49 -0400

    logger created for loginpage

--- src/test/java/com/acti/testcase/LoginPageTest.java
+++ src/test/java/com/acti/testcase/LoginPageTest.java
@@ -12,40 +12,57 @@ public class LoginPageTest extends BaseTest
 	@Test(priority = 1)
 	public void T1_LoginPageTitle()
 	{
-		//DriverManager DM = new DriverManager();
+		logger = report.createTest("logger Test login page Title");
 		String expectedtitle = lp.getTitle();
 		//Assert.assertEquals(actual,excepted);
 		System.out.println(expectedtitle);
+		logger.pass("title actual tile matched the excepted");
 		
 	}
 	//********************************************************Test Image Logo Displayed
 	@Test(priority = 2)
 	public void T2_ActiLogoDisplayed()
 	{
+		logger = report.createTest("logger Logo Image displayed")
 		boolean flag = lp.actImgdis();
 		System.out.println("The image is displayed  " + flag);
 		Assert.assertTrue(flag);
+		logger.pass("The image of Acti logo  is displayed");
 	}
 	//*********************************************************Test Link Displayed
 	@Test(priority = 3)
 	public void T3_testActiLinkDisplayes()
 	{
+		logger = report.createTest("logger Logo Acti link displayed")
+
 		boolean flag = lp.actlinkdis();
 		System.out.println("The test link is displayed  " + flag);
 		Assert.assertTrue(flag);
+		logger.pass("The actitime link is displayed");
+
 	}
 	//*******************************************************Test Login Function
 	@Test(priority = 4,enabled = false)
 	public void T4_testLoginFunction()
 	{
+		logger.create("Test Login Function");
+	
 		lp.enterUsername("admin");
+		logger.pass("Entered username");
 		lp.enterpass("manager");
+		logger.pass("Enter Password");
 		lp.clickLogin();
+		logger.pass("Clicked on login button");
 		EnterTimePage ep = new EnterTimePage();
 		String user = ep.getUserLoggedIn();
 		System.out.println("The Loggined User is " + user);
 		Assert.assertTrue(user.contains("John"));
+		logger.pass("user loggin in contains the name john");	
+
+		
 		ep.clickLogout();
+		logger.pass("Logged out form the application");	
+
  		
 	}
 	//*********************************************************Test Negative Function
