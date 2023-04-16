c42de46c62a7f9ab474789c534706bdefaa5d218
udhay <udhay@LAPTOP-FRFRGCHU>
2023-04-15 23:26:05 -0400

    Extend Report add to base test

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -2,14 +2,28 @@ package com.acti.testcase;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import com.acti.driver.DriverManager;
 import com.acti.pages.Loginpage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseTest extends DriverManager{
 	
+	protected static ExtentHtmlReporter extent;
+	protected static ExtentReports report;
+	protected ExtentTest logger;
 	Loginpage lp;
 	
+	@BeforeSuite
+	public void setUpReport()
+	{
+		 extent = new ExtentHtmlReporter("./reports/actiindex.html");
+		 report  = new ExtentReports();
+		report.attachReporter(extent);
+	}
 	@BeforeMethod
 	public void pretest()
 	{
